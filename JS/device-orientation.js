29a018282a99b829844ce08a8300b9a54c9ab581
Jianqing Bai <bai@Jianqings-MacBook-Pro.local>
2020-10-14 06:38:56 +0800

    FINAL COMMIT

--- JS/device-orientation.js
+++
@@ -1,8 +0,0 @@
-function handleOrientation(event) {
-  var oAbs = event.absolute;
-  var oA = event.alpha;
-  var oB = event.beta;
-  var oG = event.gamma;
-}
-
-window.addEventListener("deviceorientation", handleOrientation, true);
